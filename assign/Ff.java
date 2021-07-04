55a7626843a984e3ce994338b02906a7f3ebe57f
Bhavishya-P <84003405+Bhavishya-P@users.noreply.github.com>
2021-07-04 23:41:44 +0530

    Add files via upload

---
+++ assign/Ff.java
@@ -0,0 +1,25 @@
+package assign;
+import java.util.List;
+import java.util.Set;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public interface Ff
+{
+ 
+	public void initDB() throws SQLException, IOException;
+	
+	public int create(Employee e) throws SQLException;
+	public int update(Employee e) throws SQLException;
+	public int deleteByEmail(String emailId) throws SQLException;
+    public int deleteById(int id) throws SQLException;
+	
+	public Employee getEmployeeByEmail(String emailId) throws SQLException;
+	
+	public List<Employee> getAllEmployees() throws SQLException;
+	
+	
+	public Set<Employee> getAllEmployeesSortByEmailId() throws SQLException;
+	
+	public void closeDB() throws SQLException;
+}
